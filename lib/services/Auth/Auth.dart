e2e7cfb7a1345986d178ce6ed373552765e8fc43
Gonçalo Passos <dev@diogopassos.pt>
2020-05-03 15:51:45 +0100

    Changed Folder Structure of Services and created a method for social login

--- lib/services/Auth/Auth.dart
+++ lib/services/Auth/Auth.dart
@@ -1,21 +1,40 @@
-import 'dart:convert';
-
 import "package:dio/dio.dart";
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:marketgo/models/RegisterRequest.dart';
 import 'package:marketgo/models/RegisterResponse.dart';
-import 'package:marketgo/services/Exceptions.dart';
-import '../config.dart';
+import 'package:marketgo/models/SocialResponse.dart';
+import 'package:marketgo/services/Auth/Exceptions.dart';
+import 'package:marketgo/config.dart';
+
+enum SocialProvider { FACEBOOK, GOOGLE }
 
 class Auth {
   static final storage = new FlutterSecureStorage();
 
   static void authenticate(String email, String password) async {
     try {
-      var response = await Dio().post(Config.BASE_URL + "/authp",
+      var response = await Dio().post(Config.BASE_URL + "/auth",
           data: {"email": email, "password": password});
 
       await storage.write(key: "token", value: response.data);
+      print(await storage.read(key: "token"));
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  static void autenticateSocial(SocialProvider provider, String token) async {
+    var url;
+
+    if (provider == SocialProvider.FACEBOOK) {
+      url = Config.BASE_URL + "/auth/facebook";
+    } else {
+      url = Config.BASE_URL + "/auth/google";
+    }
+    try {
+      var response = await Dio().post(url, data: {"token": token});
+      var data = SocialResponse.fromJson(response.data);
+      await storage.write(key: "token", value: data.token);
       print(await storage.read(key: "token"));
     } catch (e) {
       print(e);
