307173ff4d15cb1879052428a9e6497ba7773379
Gonçalo Passos <dev@diogopassos.pt>
2020-05-04 19:56:06 +0100

    Updated RegisterResponse with User Model

--- lib/models/RegisterResponse.dart
+++ lib/models/RegisterResponse.dart
@@ -1,16 +1,20 @@
+import 'package:marketgo/models/User.dart';
+
 class RegisterResponse {
   String message;
   String field;
   String validation;
-
+  User user;
   String token;
-  RegisterResponse({this.message, this.field, this.validation, this.token});
+  RegisterResponse(
+      {this.message, this.field, this.validation, this.user, this.token});
 
   factory RegisterResponse.fromJson(Map<String, dynamic> json) {
     var regres = new RegisterResponse(
         message: json["message"],
         field: json["field"],
         validation: json['validation'],
+        user: User.fromJson(json['user']),
         token: json['token']);
     return regres;
   }
