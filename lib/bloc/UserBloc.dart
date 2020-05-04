d96340c17a318cedf929bafe848953cba48f54d5
Gonçalo Passos <dev@diogopassos.pt>
2020-05-04 22:10:52 +0100

    Added User Bloc

---
+++ lib/bloc/UserBloc.dart
@@ -0,0 +1,28 @@
+import 'dart:async';
+
+import 'package:marketgo/models/User.dart';
+
+class UserBloc {
+  static final UserBloc _singleton = UserBloc._internal();
+  User _user;
+  User get user => _user;
+
+  factory UserBloc() {
+    return _singleton;
+  }
+
+  UserBloc._internal();
+
+  final _userBlocController = StreamController<User>.broadcast();
+
+  Stream<User> get stream => _userBlocController.stream;
+
+  void setUser(User user) {
+    this._user = user;
+    _userBlocController.sink.add(user);
+  }
+
+  void dispose() {
+    _userBlocController.close();
+  }
+}
