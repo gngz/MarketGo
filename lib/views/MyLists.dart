76082884d1b7798449c4e1b6b11fbfa878ca7c32
Gonçalo Passos <dev@diogopassos.pt>
2020-05-19 14:51:54 +0100

    Added  ListView  to MyLists screen

--- lib/views/MyLists.dart
+++ lib/views/MyLists.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:marketgo/bloc/ListsBloc.dart';
 import 'package:marketgo/bloc/UserBloc.dart';
+import 'package:marketgo/models/ListModel.dart';
 import 'package:marketgo/models/User.dart';
 import 'package:marketgo/services/Auth/Auth.dart';
 
@@ -12,6 +14,68 @@ class _MyListsViewState extends State<MyListsView> {
   final ColorDarkBlue = Color(0xFF0083B0);
   final ColorLightBlue = Color(0xFF000B4DB);
   final LoginDrawer loginDrawer = LoginDrawer();
+
+  _addList(String name) {
+    ListsBloc().addList(new ListModel(name: name));
+  }
+
+  Widget _inputDialog() {
+    String listName = "";
+
+    return AlertDialog(
+      content: TextField(
+        autofocus: true,
+        decoration: new InputDecoration(
+            labelText: "Nome da Lista",
+            hintText: "e.g: Lista para Festa do João"),
+        onChanged: (value) {
+          listName = value;
+        },
+        onSubmitted: (value) {
+          _addList(value);
+          Navigator.pop(context);
+        },
+      ),
+      actions: <Widget>[
+        FlatButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: Text("Cancelar")),
+        FlatButton(
+            onPressed: () {
+              _addList(listName);
+              Navigator.pop(context);
+            },
+            child: Text("Adicionar"))
+      ],
+    );
+  }
+
+  void _showInputDialog() async {
+    await showDialog(context: context, child: _inputDialog());
+  }
+
+  Future<bool> _confirmDelete(listName) async {
+    return await showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text("Eliminar a lista $listName."),
+            content:
+                const Text("Tem a certeza de que deseja eliminar esta lista?"),
+            actions: <Widget>[
+              FlatButton(
+                  onPressed: () => Navigator.of(context).pop(true),
+                  child: const Text("Eliminar")),
+              FlatButton(
+                onPressed: () => Navigator.of(context).pop(false),
+                child: const Text("Cancelar"),
+              ),
+            ],
+          );
+        });
+  }
 
   GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey();
 
@@ -33,7 +97,58 @@ class _MyListsViewState extends State<MyListsView> {
         leading: null,
       ),
       body: Container(
-        child: Center(),
+        child: StreamBuilder<List<ListModel>>(
+            stream: ListsBloc().stream,
+            initialData: List<ListModel>(),
+            builder: (BuildContext context,
+                AsyncSnapshot<List<ListModel>> snapshot) {
+              if (snapshot.data.isEmpty) {
+                return Center(
+                  child: Text(
+                    "Que tal criar uma lista?",
+                    style: TextStyle(color: Colors.grey),
+                  ),
+                );
+              }
+              return ListView.separated(
+                itemCount: snapshot.data.length,
+                itemBuilder: (BuildContext ctxt, int index) {
+                  return Dismissible(
+                    background: Padding(
+                      padding: const EdgeInsets.only(right: 20.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: <Widget>[
+                          Icon(
+                            Icons.delete,
+                            color: Colors.red[300],
+                          ),
+                          Text(
+                            "Remover",
+                            style: TextStyle(color: Colors.red[300]),
+                          ),
+                        ],
+                      ),
+                    ),
+                    direction: DismissDirection.endToStart,
+                    key: ObjectKey(snapshot.data[index]),
+                    confirmDismiss: (direction) async {
+                      return _confirmDelete(snapshot.data[index].name);
+                    },
+                    onDismissed: (direction) {
+                      ListsBloc().removeList(snapshot.data[index]);
+                    },
+                    child: ListTile(
+                      title: Text(snapshot.data[index].name),
+                      onTap: () {},
+                      trailing: Icon(Icons.chevron_right),
+                    ),
+                  );
+                },
+                separatorBuilder: (BuildContext context, int index) =>
+                    Container(color: Colors.grey, height: 1),
+              );
+            }),
       ),
       bottomNavigationBar: BottomAppBar(
           shape: const CircularNotchedRectangle(),
@@ -50,7 +165,7 @@ class _MyListsViewState extends State<MyListsView> {
           )),
       floatingActionButton: FloatingActionButton(
           backgroundColor: ColorDarkBlue,
-          onPressed: () => {},
+          onPressed: () => {_showInputDialog()},
           tooltip: "A new button",
           child: Icon(Icons.add)),
       floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
