381b5da2cd42e6997a2ac2fe62c606ba83782e92
Gonçalo Passos <dev@diogopassos.pt>
2020-05-23 01:26:43 +0100

    Add quantity to Product model

--- lib/models/Product.dart
+++ lib/models/Product.dart
@@ -4,15 +4,30 @@ class Product {
   String description;
   double price;
   String image;
-  Product({this.ean, this.name, this.description, this.price, this.image});
+  int quantity;
+  Product({
+    this.ean,
+    this.name,
+    this.description,
+    this.price,
+    this.image,
+    this.quantity,
+  });
 
   factory Product.fromJson(Map<String, dynamic> json) {
     var object = new Product(
-        ean: json["ean"],
-        name: json["name"],
-        description: json["description"],
-        price: json["price"],
-        image: json["image"]);
+      ean: json["ean"],
+      name: json["name"],
+      description: json["description"],
+      price: json["price"],
+      image: json["image"],
+      quantity: json["pivot"]["quantity"],
+    );
+
     return object;
+  }
+
+  static List<Product> fromList(List json) {
+    return json.map((e) => Product.fromJson(e)).toList();
   }
 }
