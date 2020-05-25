85aa7c41a14c5c159de572522ce72796297aa653
Manel Pereira <afonso.pereira4525@gmail.com>
2020-05-25 20:32:25 +0100

    Removed comments

--- lib/services/ApiService.dart
+++ lib/services/ApiService.dart
@@ -2,6 +2,8 @@ import 'package:dio/dio.dart';
 import 'package:marketgo/bloc/UserBloc.dart';
 import 'package:marketgo/config.dart';
 
+typedef void OnTimeoutCallback(String title);
+
 class ApiService {
   static ApiService _instance = ApiService._internal();
   ApiService._internal();
@@ -9,15 +11,16 @@ class ApiService {
   factory ApiService() {
     return _instance;
   }
-
   Dio getHttp() {
     var dio = new Dio();
     dio.options.baseUrl = Config().baseUrl;
-    dio.options.connectTimeout = 5 * 1000; //2 secs
-    dio.interceptors.add(InterceptorsWrapper(onError: (DioError e) async {
-      //mostrar um toast de erro, com inversão de dependências era capaz de se tornar fixe...
-      return e;
-    }));
+    //dio.options.connectTimeout = 5 * 1000; //2 secs
+    /* if (callback != null) {
+      dio.interceptors.add(InterceptorsWrapper(onError: (DioError e) async {
+        callback("Não foi possivel");
+        return e;
+      }));
+    } */
     return dio;
   }
 
