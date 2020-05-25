fda87000e00641b570f9a2c3a7940db954ea0b49
Gonçalo Passos <dev@diogopassos.pt>
2020-05-25 21:40:00 +0100

    Created Card Model

---
+++ lib/models/CardModel.dart
@@ -0,0 +1,32 @@
+class CardModel {
+  String id;
+  String brand;
+  int expMonth;
+  int expYear;
+  String lastFour;
+  String cardHolder;
+
+  CardModel({
+    this.id,
+    this.brand,
+    this.expMonth,
+    this.expYear,
+    this.lastFour,
+    this.cardHolder,
+  });
+
+  static List<CardModel> fromList(List json) {
+    return json.map((e) => CardModel.fromJson(e)).toList();
+  }
+
+  factory CardModel.fromJson(Map<String, dynamic> json) {
+    return new CardModel(
+      id: json['id'],
+      brand: json['brand'],
+      expMonth: json['exp_month'] as int,
+      expYear: json['exp_year'] as int,
+      lastFour: json['last4'],
+      cardHolder: json['name'],
+    );
+  }
+}
