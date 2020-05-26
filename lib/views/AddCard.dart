f4410859d6688eae60914ea250bc8af7503c1177
Gonçalo Passos <dev@diogopassos.pt>
2020-05-26 01:25:13 +0100

    created AddCard view

---
+++ lib/views/AddCard.dart
@@ -0,0 +1,226 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:marketgo/models/CardModel.dart';
+import 'package:mask_text_input_formatter/mask_text_input_formatter.dart';
+
+final cvcFormatter =
+    new MaskTextInputFormatter(mask: '###', filter: {"#": RegExp(r'[0-9]')});
+
+final expFormatter =
+    new MaskTextInputFormatter(mask: '##/##', filter: {"#": RegExp(r'[0-9]')});
+
+final cvcControler = TextEditingController();
+
+final ccFormatter = new MaskTextInputFormatter(
+    mask: "#### #### #### ####", filter: {"#": RegExp(r'[0-9]')});
+
+class AddCardView extends StatefulWidget {
+  @override
+  _AddCardViewState createState() => _AddCardViewState();
+}
+
+class _AddCardViewState extends State<AddCardView> {
+  CardModel card = new CardModel();
+
+  Widget _getImageByCardBrand(String brand) {
+    switch (brand.toLowerCase()) {
+      case "visa":
+        return Image.asset("assets/cards/visa.png");
+      case "mastercard":
+        return Image.asset("assets/cards/mastercard.png");
+      case "american express":
+        return Image.asset("assets/cards/amex.png");
+      case "diners club":
+        return Image.asset("assets/cards/diners.png");
+      case "discover":
+        return Image.asset("assets/cards/discovery.png");
+      case "jcb":
+        return Image.asset("assets/cards/jcb.png");
+      default:
+        return const SizedBox();
+    }
+  }
+
+  String getCardType(String cardNumber) {
+    final amexCard = {"regex": RegExp(r"^3[47]"), "brand": "american express"};
+    final dinersCard = {
+      "regex": RegExp(r"^3(?:0[0-5]|[68])"),
+      "brand": "diners club"
+    };
+    final discoverCard = {"regex": RegExp(r"^6(?:011|5)"), "brand": "discover"};
+    final jcbCard = {"regex": RegExp(r":^(?:2131|1800|35)"), "brand": "jcb"};
+    final masterCard = {
+      "regex": RegExp(
+          r"^(?:5[1-5]|222[1-9]|22[3-9][0-9]|2[3-6][0-9]{2}|27[01][0-9]|2720)"),
+      "brand": "mastercard"
+    };
+    final visaCard = {"regex": RegExp(r"^4"), "brand": "visa"};
+
+    var validators = [
+      amexCard,
+      dinersCard,
+      discoverCard,
+      jcbCard,
+      masterCard,
+      visaCard
+    ];
+
+    for (int i = 0; i < validators.length; i++) {
+      RegExp reg = validators[i]["regex"];
+      String brand = validators[i]["brand"];
+
+      if (reg.hasMatch(cardNumber)) {
+        return brand;
+      }
+    }
+
+    return null;
+  }
+
+  Widget drawCard(CardModel card) {
+    return Container(
+        height: 200,
+        margin: EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15),
+          boxShadow: [
+            BoxShadow(
+                color: Colors.black.withOpacity(0.5),
+                spreadRadius: 0,
+                blurRadius: 5,
+                offset: Offset(5, 5)),
+          ],
+          gradient:
+              LinearGradient(colors: [Color(0xFF434343), Color(0xFF211D1D)]),
+        ),
+        child: Stack(
+          children: <Widget>[
+            Positioned(
+                bottom: 20,
+                left: 20,
+                child: Row(
+                  children: <Widget>[
+                    Text(
+                      card.cardHolder ?? "Card Holder",
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w300,
+                        color: Colors.white,
+                      ),
+                    ),
+                    Text(
+                      "  ${card.expMonth ?? DateTime.now().month.toString().padLeft(2, "0")}/${card.expYear ?? DateTime.now().year.toString().substring(2)}",
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
+                )),
+            Positioned(
+              bottom: 60,
+              left: 20,
+              child: Text(
+                card.lastFour ?? "1234 5678 8765 4321",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.w300),
+              ),
+            ),
+            Positioned(
+              right: 10,
+              bottom: 0,
+              height: 50,
+              child: _getImageByCardBrand(card.brand ?? ""),
+            ),
+          ],
+        ));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Adicionar Cartão"),
+      ),
+      body: Form(
+        child: Column(
+          children: <Widget>[
+            drawCard(card),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                child: ListView(
+                  children: <Widget>[
+                    TextFormField(
+                      onChanged: (value) {
+                        setState(() {
+                          card.cardHolder = value;
+                        });
+                      },
+                      decoration: InputDecoration(
+                        labelText: "Titular do Cartão",
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    TextFormField(
+                      inputFormatters: [ccFormatter],
+                      decoration: InputDecoration(
+                        labelText: "Número do Cartão",
+                      ),
+                      onChanged: (value) {
+                        setState(() {
+                          card.lastFour = value;
+                          card.brand = getCardType(value);
+                        });
+                      },
+                    ),
+                    const SizedBox(height: 10),
+                    TextFormField(
+                        inputFormatters: [expFormatter],
+                        decoration: InputDecoration(
+                          labelText: "Data de Vencimento",
+                        ),
+                        onChanged: (value) {
+                          print(value);
+
+                          var splited = value.split("/");
+
+                          if (splited.length > 1) {
+                            print("contains!! ${splited[0]}");
+                            print(splited);
+                            var month = splited[0];
+                            var year = splited[1];
+                            setState(() {
+                              card.expMonth = int.parse(month);
+                              card.expYear = int.parse(year);
+                            });
+                          } else {
+                            setState(() {
+                              var month = splited[0];
+                              card.expMonth = int.parse(month);
+                              card.expYear = null;
+                            });
+                          }
+                        }),
+                    const SizedBox(height: 10),
+                    TextFormField(
+                      inputFormatters: [cvcFormatter],
+                      controller: cvcControler,
+                      decoration: InputDecoration(
+                        labelText: "CVC",
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton:
+          FloatingActionButton(onPressed: () {}, child: Icon(Icons.done)),
+    );
+  }
+}
