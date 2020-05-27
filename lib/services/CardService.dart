9bb42650916f566d00dab9579e09a479ffbff9b7
Manel Pereira <afonso.pereira4525@gmail.com>
2020-05-27 01:56:39 +0100

    Added method pay to cardservice

--- lib/services/CardService.dart
+++ lib/services/CardService.dart
@@ -60,4 +60,15 @@ class CardService {
       return null;
     }
   }
+
+  Future<bool> pay(List<String> productIds, String cardId, int listId) async {
+    try {
+      var response = await ApiService().getAuthHttp().post("/payments/",
+          data: {"products": productIds, "card_id": cardId, "list_id": listId});
+      if (response.statusCode == 200) return true;
+    } catch (e) {
+      print(e);
+    }
+    return false;
+  }
 }
